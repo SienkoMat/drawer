fef90e50bf3c914c5c9dd28ec0b5a2a2c43c1de0
SienkoMat <mateusz.sienko1@gmail.com>
2023-11-14 20:31:44 +0100

    Added possibility to set an upper bound.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,13 +16,19 @@ public final class Main {
 
         Bracket choosenBracket = new Bracket();
         Bracket randomBracket = new Bracket();
+
         choosenBracket.setSizeOfBracket();
+        choosenBracket.setUpperBound();
         choosenBracket.ChooseSet();
         choosenBracket.getSet();
 
         randomBracket.setSizeOfBracket(choosenBracket.getSizeOfBracket());
+        randomBracket.setUpperBound(choosenBracket.getUpperBound());
         randomBracket.RandomSet();
         randomBracket.getSet();
+
+        choosenBracket.commonElements(randomBracket.bracket);
+
     }
 
 }
@@ -30,10 +36,11 @@ public final class Main {
 class Bracket {
     Scanner scanner = new Scanner(System.in);
     int sizeOfBracket;
+    int upperBound;
     String user;
 
     public int setSizeOfBracket() {
-        System.out.println("Podaj ile liczb chcesz wylosować");
+        System.out.println("Podaj ile liczb chcesz wylosować.");
         sizeOfBracket = scanner.nextInt();
         return sizeOfBracket;
     }
@@ -47,25 +54,59 @@ class Bracket {
         return sizeOfBracket;
     }
 
+    public int setUpperBound() {
+        System.out.println("Podaj górny zakres losowanych liczb.");
+        upperBound = scanner.nextInt();
+        return upperBound;
+    }
+
+    public int setUpperBound(int upperBound) {
+        this.upperBound = upperBound;
+        return upperBound;
+    }
+
+    public int getUpperBound() {
+        return upperBound;
+    }
+
     Set<Integer> bracket = new HashSet<Integer>(sizeOfBracket);
 
     public void ChooseSet() {
         for (int i = 0; i < sizeOfBracket; i++) {
-            System.out.println("Podaj " + (i + 1) + ". z " + sizeOfBracket + " liczb");
+            System.out.println(
+                    "Podaj " + (i + 1) + ". z " + sizeOfBracket + " liczb w zakresie od 1 do " + upperBound + ".");
             int tempInt = scanner.nextInt();
-            bracket.add(tempInt);
+
+            if ((!(this.bracket.contains(tempInt))) && tempInt > 0 && tempInt <= upperBound)
+                bracket.add(tempInt);
+            else {
+                System.out.println("Ta liczba nie spełnia założonych warunków");
+                i--;
+            }
         }
     }
 
     public void RandomSet() {
+        Random random = new Random();
         for (int i = 0; i < sizeOfBracket; i++) {
-            Random random = new Random();
-            bracket.add(random.nextInt(1, 9));
+            int tempInt = random.nextInt(1, this.upperBound + 1);
+            if (!(this.bracket.contains(tempInt)))
+                bracket.add(tempInt);
+            else
+                i--;
+
         }
     }
 
     public Set<Integer> getSet() {
         System.out.println(bracket);
         return bracket;
+    }
+
+    public Set<Integer> commonElements(Set<Integer> bracket) {
+        Set<Integer> commonBracket = new HashSet<Integer>(this.bracket);
+        commonBracket.retainAll(bracket);
+        System.out.println("Wspólne liczby z setu to: " + this.bracket);
+        return commonBracket;
     }
 }
